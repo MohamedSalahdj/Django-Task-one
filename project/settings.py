cc04cc229b1bcc8d680fd509eb981d596c34464c
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-08-20 23:02:47 +0300

    installed taggit library and add it in installed_app

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #myapps
     'question',
+    "taggit",
 ]
 
 MIDDLEWARE = [
--- question/models.py
+++ question/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+from django.utils import timezone
 
 # Create your models here.
-
